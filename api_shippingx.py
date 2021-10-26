fe78651c37669c9bb1e38279b705c11019a7e253
Billz <58287516+billz94@users.noreply.github.com>
2021-10-26 07:02:45 +0200

    Update api_shippingx.py

--- api_shippingx.py
+++ api_shippingx.py
@@ -1,4 +1,4 @@
-import requests
+666import requests
 import json
 import platform
 import subprocess
@@ -28,7 +28,7 @@ def alert(url, params):
     r = requests.post(url, json=params, headers=headers)
     return r
 
-recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999453942", "+265880756149", "+265999755473", "+265991351754", "+265997762346"]
+recipients = ["+265998006237", "+265991312980", "+265995246144", "+265995628519", "+265999959499","+265995316633"]
 
 cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/1')
 
